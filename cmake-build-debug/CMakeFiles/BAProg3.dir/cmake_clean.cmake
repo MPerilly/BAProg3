b8c4159a838b35b60927f2e496c2e7e76ef3a60c
Michael Perillo <msp501@nyu.edu>
2020-07-07 16:56:05 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/BAProg3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BAProg3.exe"
+  "BAProg3.exe.manifest"
+  "BAProg3.lib"
+  "BAProg3.pdb"
+  "CMakeFiles/BAProg3.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BAProg3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
